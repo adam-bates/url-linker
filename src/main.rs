eb2c33bbc52f92fce5b332590c742f573de72a5d
Adam Bates <adam@adambates.ca>
2022-03-20 01:06:30 -0400

    Configured db connection.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,21 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket_sync_db_pools::{database, postgres};
+
 mod config;
 mod controllers;
+
+#[database("url_linker")]
+struct DbConnection(postgres::Client);
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
     config::init_env();
 
     let rocket = rocket::build();
+    let rocket = rocket.attach(DbConnection::fairing());
     let rocket = controllers::mount(rocket);
 
-    return match rocket.launch().await {
-        Err(err) => match err.kind() {
-            rocket::error::ErrorKind::Collisions(collisions) => {
-                for r in &collisions.routes {
-                    println!("\nRoute1: {:?}\n\nRoute2: {:?}\n", r.0, r.1);
-                }
-                return Ok(());
-            }
-            _ => Err(err),
-        },
-        x => x,
-    };
+    return rocket.launch().await;
 }
