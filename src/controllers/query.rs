2e97b0528f43610ccfa95b5a028341fb0d19377c
Adam Bates <adam@adambates.ca>
2022-03-20 01:09:07 -0400

    Configured db connection. (#4)

--- src/controllers/query.rs
+++ src/controllers/query.rs
@@ -6,7 +6,9 @@ pub fn mount(rocket: Rocket<Build>) -> Rocket<Build> {
     return rocket.mount("/", routes![query]);
 }
 
-#[get("/<query..>")]
-fn query(query: PathBuf) -> String {
-    return format!("query: {:?}", query);
+#[get("/<key..>")]
+fn query(key: PathBuf) -> String {
+    let key = key.display().to_string();
+
+    return format!("key: {key}");
 }
