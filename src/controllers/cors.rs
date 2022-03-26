eb68d6f451d094548d093f8a35bc0f817170facf
Adam Bates <adam@adambates.ca>
2022-03-26 10:55:53 -0400

    Api (#13)
    * Converted swagger.json to swagger.yaml in order to fix bug with requirements for auth.
    
    * Added CORS fairing.

---
+++ src/controllers/cors.rs
@@ -0,0 +1,31 @@
+use rocket::{
+    fairing::{Fairing, Info, Kind},
+    http::Header,
+    Build, Request, Response, Rocket,
+};
+
+pub fn attach(rocket: Rocket<Build>) -> Rocket<Build> {
+    return rocket.attach(CORS);
+}
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PUT, DELETE, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
--- src/controllers/mod.rs
+++ src/controllers/mod.rs
@@ -1,12 +1,15 @@
 use rocket::{Build, Rocket};
 
 mod api;
+mod cors;
 mod guards;
 mod query;
 mod responders;
 mod types;
 
 pub fn mount(rocket: Rocket<Build>) -> Rocket<Build> {
+    let rocket = cors::attach(rocket);
+
     let rocket = api::mount(rocket);
     let rocket = query::mount(rocket);
 
